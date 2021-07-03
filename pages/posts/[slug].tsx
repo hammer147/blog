d75e6d1717b90b5a92723bb3a9ef3c83092550c3
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-07-03 15:58:48 +0200

    added getStaticProps and getStaticPaths

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,8 +1,38 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
 import React from 'react'
 import PostContent from '../../components/posts/post-detail/post-content'
+import { getPostData, getPostsFiles } from '../../lib/posts-util'
+import { Post } from '../../typings'
 
-function PostDetailPage() {
+type Props = {
+  post: Post
+}
+
+function PostDetailPage({ post }: Props) {
   return <PostContent />
+}
+
+export const getStaticProps: GetStaticProps = async context => {
+  const { params } = context
+  const { slug } = params!
+
+  const postData = getPostData(slug as string)
+
+  return {
+    props: {
+      post: postData
+    },
+    revalidate: 600
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async context => {
+  const postFilenames = getPostsFiles()
+  const slugs = postFilenames.map(fileName => fileName.replace(/\.md$/, ''))
+  return {
+    paths: slugs.map(slug => ({ params: { slug } })),
+    fallback: false
+  }
 }
 
 export default PostDetailPage
