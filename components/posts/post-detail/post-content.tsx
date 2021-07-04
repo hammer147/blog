f0a1ba8c668b1fd0f5bafef904b84de4528e3a9a
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-07-04 18:27:42 +0200

    added customRenderer

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -2,6 +2,7 @@ import ReactMarkdown from 'react-markdown'
 import { Post } from '../../../typings'
 import classes from './post-content.module.css'
 import PostHeader from './post-header'
+import Image from 'next/image'
 
 type Props = {
   post: Post
@@ -11,10 +12,37 @@ function PostContent({ post }: Props) {
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`
 
+  // We want to use the Image component from Next.js
+  // img elements are wrapped in p by react-markdown
+  // Just replacing img with Image would cause a warning about a div in a p
+  // therefor we replace each p that wraps an img with a div that wraps an Image
+  // We are not using typings here because it is quite complex:
+  // https://stackoverflow.com/questions/66628783/react-markdown-type-for-paragraph-renderer
+
+  const customRenderers = {
+    p(paragraph: any) {
+      const { node, children } = paragraph
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0]
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        )
+      }
+      return <p>{children}</p>
+    }
+  }
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   )
 }
