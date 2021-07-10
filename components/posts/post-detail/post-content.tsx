68e49720e1ba9b09426342b1d8302de7c43ad800
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-07-10 16:27:31 +0200

    added react-syntax-highlighter

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -3,6 +3,8 @@ import { Post } from '../../../typings'
 import classes from './post-content.module.css'
 import PostHeader from './post-header'
 import Image from 'next/image'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 type Props = {
   post: Post
@@ -36,6 +38,15 @@ function PostContent({ post }: Props) {
         )
       }
       return <p>{children}</p>
+    },
+    code(code: any) {
+      const { className, children } = code
+      const language = className.split('-')[1] // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter style={vscDarkPlus} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      )
     }
   }
 
