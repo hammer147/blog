481b6d7fcb2fda36d00746a013b9f071a47fa569
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-07-11 14:31:39 +0200

    send data to api route

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -1,23 +1,38 @@
+import { FormEvent, useState } from 'react'
 import classes from './contact-form.module.css'
 
 function ContactForm() {
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [message, setMessage] = useState('')
+
+  function sendMessageHandler(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    // optional: add client-side validation
+    fetch('/api/contact', {
+      method: 'POST',
+      body: JSON.stringify({ email, name, message }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" required />
+            <input type="email" id="email" required value={email} onChange={e => setEmail(e.target.value)} />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" required />
+            <input type="text" id="name" required value={name} onChange={e => setName(e.target.value)} />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Message</label>
-          <textarea id="message" rows={5}></textarea>
+          <textarea id="message" rows={5} required value={message} onChange={e => setMessage(e.target.value)}></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
