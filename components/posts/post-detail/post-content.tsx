48f25e9a8fe4cc51c6747aa593e8712c8b3fd3c7
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-07-25 15:53:37 +0200

    react-syntax-highlighter light build

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -3,8 +3,16 @@ import { Post } from '../../../typings'
 import classes from './post-content.module.css'
 import PostHeader from './post-header'
 import Image from 'next/image'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import vscDarkPlus from 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus'
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
+import ts from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript'
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
+
+SyntaxHighlighter.registerLanguage('js', js)
+SyntaxHighlighter.registerLanguage('ts', ts)
+SyntaxHighlighter.registerLanguage('css', css)
 
 type Props = {
   post: Post
